6b061c42daaf37d5623ee862c87476a393089579
Urban <urban.vogrin@ssrd.io>
2019-11-27 16:24:02 +0100

    added sample

---
+++ samples/IdentityUI.Sample/IdentityUI.Sample/Services/EmailSender.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityUI.Sample.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly ILogger<EmailSender> _logger;
+
+        public EmailSender(ILogger<EmailSender> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            _logger.LogInformation($"Email for {email}, Body {htmlMessage}");
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ samples/IdentityUI.Sample/README.md
@@ -0,0 +1,7 @@
+This sample uses InMemory Database and does not send mails but just writes them in logs.
+
+User:
+```
+username: 'admin'
+password: 'Password'
+```
